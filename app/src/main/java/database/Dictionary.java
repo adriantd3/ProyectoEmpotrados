bf54f3e3ee4edaa152f9f9a77aace2ad62469a60
Ezequiel Sánchez <124664474+ezee-sg@users.noreply.github.com>
2024-12-27 19:33:49 +0100

    Dictionary redirects to operations methods

--- app/src/main/java/database/Dictionary.java
+++ app/src/main/java/database/Dictionary.java
@@ -6,13 +6,24 @@ import android.database.sqlite.SQLiteDatabase;
 import java.util.ArrayList;
 import java.util.List;
 
+import api.nominatim.GeocodingResponse;
+import database.dto.NewDestiny;
+import database.dto.NewTrip;
+import database.operations.DateInfoOperations;
+import database.operations.DestinyOperations;
+import database.operations.TripOperations;
 import database.tables.InterfaceTable;
 import database.tables.DateInfoTable;
 import database.tables.DestinyTable;
 import database.tables.TripTable;
 
+import database.entities.*;
+
 public class Dictionary {
     private SQLiteDatabase db;
+    private TripOperations tripOperations;
+    private DestinyOperations destinyOperations;
+    private DateInfoOperations dateInfoOperations;
 
     public Dictionary(Context context, String dbName) {
         List<InterfaceTable> tables = new ArrayList<>();
@@ -21,7 +32,69 @@ public class Dictionary {
         tables.add(new DateInfoTable());
         DictDbHelper dbHelper = new DictDbHelper(context, tables);
         this.db = dbHelper.getWritableDatabase();
+
+        this.tripOperations = new TripOperations();
+        this.destinyOperations = new DestinyOperations();
+        this.dateInfoOperations = new DateInfoOperations();
     }
 
-    // TODO: add operations methods from operations package
+    // TripEntity methods
+    public TripEntity getTripById(Integer id) {
+        return tripOperations.getTripById(db, id);
+    }
+
+    public List<TripEntity> getAllTrips() {
+        return tripOperations.getAllTrips(db);
+    }
+    public void insertTrip(NewTrip trip) {
+        tripOperations.insertTrip(db, trip);
+    }
+
+    public void updateTrip(TripEntity trip) {
+        tripOperations.updateTrip(db, trip);
+    }
+
+    public void deleteTrip(Integer id) {
+        tripOperations.deleteTrip(db, id);
+    }
+
+    // DestinyEntity methods
+    public List<DestinyEntity> getAllByTripId(Integer tripId) {
+        return destinyOperations.getAllByTripId(db, tripId);
+    }
+    public DestinyEntity getDestinyById(Integer id) {
+        return destinyOperations.getDestinyById(db, id);
+    }
+
+    public void insertDestiny(NewDestiny destiny, GeocodingResponse response) {
+        destinyOperations.insertDestiny(db, destiny, response);
+    }
+
+    public void updateDestiny(DestinyEntity destiny, GeocodingResponse geocoding) {
+        destinyOperations.updateDestiny(db, destiny, geocoding);
+    }
+
+    public void deleteDestiny(Integer id) {
+        destinyOperations.deleteDestiny(db, id);
+    }
+
+    // DateInfoEntity methods
+    public List<DateInfoEntity> getDateInfoByDestinyId(Integer destinyId) {
+        return dateInfoOperations.getDateInfoByDestinyId(db, destinyId);
+    }
+
+    public DateInfoEntity getDateInfoById(Integer id) {
+        return dateInfoOperations.getDateInfoById(db, id);
+    }
+    public void insertDateInfo(DateInfoEntity dateInfo) {
+        dateInfoOperations.insertDateInfo(db, dateInfo);
+    }
+
+    public void updateDateInfo(DateInfoEntity dateInfo) {
+        dateInfoOperations.updateDateInfo(db, dateInfo);
+    }
+
+    public void deleteDateInfo(Integer id) {
+        dateInfoOperations.deleteDateInfo(db, id);
+    }
 }
