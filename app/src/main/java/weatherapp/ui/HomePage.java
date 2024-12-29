54bd91b5f0c80ef9304437817728fdc63148e1ea
Adrián Torremocha Doblas <adriantorremocha@hotmail.com>
2024-12-29 17:09:17 +0100

    Arreglos varios

--- app/src/main/java/weatherapp/ui/HomePage.java
+++ app/src/main/java/weatherapp/ui/HomePage.java
@@ -2,8 +2,12 @@ package weatherapp.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import database.Dictionary;
@@ -13,6 +17,7 @@ import ssedm.lcc.example.newdictionarywithddbb.SingletonMap;
 
 public class HomePage extends AppCompatActivity {
     public static final String SHARED_AGENDA = "SHARED_AGENDA";
+    private ActivityResultLauncher<Intent> launcher;
 
     private Dictionary dict;
 
@@ -21,7 +26,13 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+        initNewTripLauncher();
+
         initDictionary();
+    }
+
+    private void loadTripList() {
+        // Cargar la lista de viajes
     }
 
     public void onClick(View view) {
@@ -36,21 +47,34 @@ public class HomePage extends AppCompatActivity {
     }
 
     private void redirectToNewTrip() {
-        Intent it = new Intent(this, NewTrip.class);
-        startActivity(it);
+        launcher.launch(new Intent(this, NewTrip.class));
     }
 
     // Temporary redirect to NewDestination
     private void redirectToNewDestination() {
-        Intent it = new Intent(this, NewDestination.class);
-        startActivity(it);
+        launcher.launch(new Intent(this, NewDestination.class));
     }
 
     private void initDictionary() {
         dict = (Dictionary) SingletonMap.getInstance().get(MainActivity.SHARED_AGENDA);
-        if(dict == null) {
+        if (dict == null) {
             dict = new Dictionary(getApplicationContext());
             SingletonMap.getInstance().put(HomePage.SHARED_AGENDA, dict);
         }
+    }
+
+    private void initNewTripLauncher() {
+        launcher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null && data.getBooleanExtra("created", false)) {
+                            Toast toast = Toast.makeText(this, R.string.trip_added, Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                    }
+                }
+        );
     }
 }
--- app/src/main/java/weatherapp/ui/NewTrip.java
+++ app/src/main/java/weatherapp/ui/NewTrip.java
@@ -1,12 +1,17 @@
 package weatherapp.ui;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 
 import database.Dictionary;
@@ -20,14 +25,14 @@ public class NewTrip extends AppCompatActivity {
     //ADD DATABASE CONNECTION
     Dictionary dict;
 
-    TextInputEditText tripName;
+    EditText tripName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_trip);
 
-        tripName = findViewById(R.id.trip_name_input);
+        tripName = findViewById(R.id.editTextTrip);
         initDictionary();
 
         if(getSupportActionBar() != null) {
@@ -48,19 +53,36 @@ public class NewTrip extends AppCompatActivity {
 
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.cancel_button: goBack(); break;
+            case R.id.cancel_button: finish(); break;
             case R.id.save_button: createTrip(); break;
         }
     }
 
-    private void goBack() {
-        Intent it = new Intent(this, MainActivity.class);
-        startActivity(it);
-    }
-
     private void createTrip() {
         String name = tripName.getText().toString();
+        if(name.isEmpty()) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setCancelable(false);
+            builder.setTitle(R.string.error);
+            builder.setIcon(android.R.drawable.ic_dialog_alert);
+            builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+
+            builder.setMessage(R.string.trip_name_error);
+            builder.show();
+
+            return;
+        }
         dict.insertTrip(new NewTripDTO(name));
+
+
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("created", true); // Indica que se creó correctamente
+        setResult(RESULT_OK, resultIntent);
 
         finish();
     }
